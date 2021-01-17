82f779bb4de2d6470607446ed69c4c662483d572
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-17 13:02:53 +0000

    add more poc code

--- lua/BetterUI/GUI/GUIMarineHUD.lua
+++ lua/BetterUI/GUI/GUIMarineHUD.lua
@@ -2,6 +2,32 @@ local startMouseX = 0
 local startMouseY = 0
 local startPos = nil
 
+local uiElementsToMove
+
+local oldInit = GUIMarineHUD.Initialize
+function GUIMarineHUD:Initialize()
+    oldInit(self)
+
+    uiElementsToMove = {
+        {Element = self.statusDisplays.statusBackground},
+        {Element = self.nanoshieldText, NoScale = true},
+        {Element = self.commanderName},
+        {Element = self.locationText},
+        {Element = self.armorLevel, NoScale = true},
+        {Element = self.weaponLevel, NoScale = true},
+        {Element = self.statusDisplay.statusbackground},
+        {Element = self.gameTime},
+    }
+end
+
+local function ProcessMove(self, mouseX, mouseY, scale)
+    local pos = self.Element:GetPosition()
+    local deltaX = (mouseX - self.StartMouseX) / scale
+    local deltaY = (mouseY - self.StartMouseY) / scale
+    local newPos = Vector(self.StartPos.x + deltaX, self.StartPos.y + deltaY, 0)
+    self.Element:SetPosition(newPos)
+end
+
 local oldOnUpdate = GUIMarineHUD.Update
 function GUIMarineHUD:Update(deltaTime)
     oldOnUpdate(self, deltaTime)
@@ -11,26 +37,26 @@ function GUIMarineHUD:Update(deltaTime)
     end
 
     local mouseX, mouseY = Client.GetCursorPosScreen()
+    local screenWidth = Client.GetScreenWidth()
+    local screenHeight = Client.GetScreenHeight()
 
-    -- Process mouse hover
-    if not self.mouseDown then
-        local statusDisplaysPosition = self.statusDisplays.statusBackground:GetScreenPosition(Client.GetScreenWidth(), Client.GetScreenHeight())
-        local statusDisplaysX = statusDisplaysPosition.x
-        local statusDisplaysY = statusDisplaysPosition.y
-        local width = self.statusDisplays.statusBackground.guiItem:GetTextureWidth()
-        local height = self.statusDisplays.statusBackground.guiItem:GetTextureHeight()
-    
-        if mouseX >= statusDisplaysX and mouseX <= statusDisplaysX + width and mouseY >= statusDisplaysY and mouseY <= statusDisplaysY + height then
-            self.hoverElement = self.statusDisplays
-        else
-            self.hoverElement = nil
+    -- Process clicks
+    if self.mouseDown then
+        if self.hoverElement and self.hoverElement.StartPos then
+            local scale = ConditionalValue(self.hoverElement.NoScale, 1, self.scale)
+            ProcessMove(self.hoverElement, mouseX, mouseY, scale)
         end
     else
-        -- Process click n drag
-        if self.hoverElement and startPos then
-            local pos = self.statusDisplays.statusBackground:GetPosition()
-            local newPos = Vector(startPos.x + ((mouseX - startMouseX) / self.scale), startPos.y + ((mouseY - startMouseY) / self.scale), 0)
-            self.statusDisplays.statusBackground:SetPosition(newPos)
+        -- Process hover
+        self.hoverElement = nil
+        for _, element in ipairs(uiElementsToMove) do
+            local pos = element.Element:GetScreenPosition(screenWidth, screenHeight)
+            local size = element.Element:GetSize()
+
+            if mouseX >= pos.x and mouseX <= pos.x + size.x and mouseY >= pos.y and mouseY <= pos.y + size.y then
+                self.hoverElement = element
+                break
+            end
         end
     end
 end
@@ -38,30 +64,16 @@ end
 function GUIMarineHUD:SendKeyEvent(key, down)
     if not PlayerUI_GetBetterUIEnabled() then return false end
 
-    if key == InputKey.MouseButton0 then
+    if key == InputKey.MouseButton0 and self.hoverElement then
         if self.mouseDown ~= down then
             if down then
-                startMouseX, startMouseY = Client.GetCursorPosScreen()
-                startPos = self.statusDisplays.statusBackground:GetPosition()
+                self.hoverElement.StartMouseX, self.hoverElement.StartMouseY = Client.GetCursorPosScreen()
+                self.hoverElement.StartPos = self.hoverElement.Element:GetPosition()
             else
-                startMouseX, startMouseY, startPos = 0, 0, nil
+                self.hoverElement.StartMouseX, self.hoverElement.StartMouseY, self.hoverElement.StartPos = 0, 0, nil
             end
         end
         self.mouseDown = down
-        -- if self.hoverElement then
-        --     local mouseX, mouseY = Client.GetCursorPosScreen()
-        --     local pos = self.statusDisplays.statusBackground:GetPosition()
-        --     if self.mouseDown ~= down then
-        --         startMouseX = mouseX
-        --         startMouseY = mouseY
-        --         startPos = pos
-        --     end
-
-        --     self.mouseDown = down
-        --     print("CLICKED ON STATUS DISPLAYS")
-        --     local newPos = Vector(startPos.x + mouseX - startMouseX, startPos.y + mouseY - startMouseY, 0)
-        --     self.statusDisplays.statusBackground:SetPosition(newPos)
-        -- end
     end
 
     if key == InputKey.Escape and not down then
