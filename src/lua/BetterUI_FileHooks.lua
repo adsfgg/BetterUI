d5bedd36977a4e5dbde84fccd3132fcf09fd2a09
adsfgg <11364579+adsfgg@users.noreply.github.com>
2022-09-28 23:47:29 +0100

    Add GUIExoHUD, remvoe playerStatusIcons to prevent script errors

--- src/lua/BetterUI_FileHooks.lua
+++ src/lua/BetterUI_FileHooks.lua
@@ -4,6 +4,7 @@ ModLoader.SetupFileHook("lua/GUIScoreboard.lua", "lua/BetterUI/GUI/GUIScoreboard
 
 -- HUD
 ModLoader.SetupFileHook("lua/Hud/Marine/GUIMarineHUD.lua", "lua/BetterUI/HUD/Marine/GUIMarineHUD.lua", "replace")
+ModLoader.SetupFileHook("lua/Hud/Marine/GUIExoHUD.lua", "lua/BetterUI/HUD/Marine/GUIExoHUD.lua", "replace")
 ModLoader.SetupFileHook("lua/GUIAlienHUD.lua", "lua/BetterUI/HUD/Alien/GUIAlienHUD.lua", "replace")
 
 -- Rest
